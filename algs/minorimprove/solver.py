817c4cd78262babc24da6921da27f4c9b4b3459b
Matthew Tran <matthewlamtran@berkeley.edu>
2019-12-06 16:01:46 -0800

    Ran minorimprove on remaining long ones

--- algs/minorimprove/solver.py
+++ algs/minorimprove/solver.py
@@ -10,6 +10,8 @@ import brute
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
+import optitsp
+
 from student_utils import *
 """
 ======================================================================
@@ -103,58 +105,7 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
                 listdropoffs[i[0]] = i[1]
 
     # Run TSP to get a better cycle
-    listdps = list(listdropoffs.keys()) # contains all points that must be gone through
-    if starting_car_location not in listdps:
-        listdps.append(starting_car_location)
-
-    dpmap = {i:listdps[i] for i in range(len(listdps))}
-    data_dist = [[0 for _ in range(len(dpmap))] for _ in range(len(dpmap))]
-    for i in range(len(data_dist)):
-        for j in range(len(data_dist)):
-            data_dist[i][j] = int(all_paths[dpmap[i]][dpmap[j]]*1000000) # OR tools only uses integers
-    data_num_vehicles = 1
-    data_depot = -1 # starting location
-    for key in dpmap:
-        if dpmap[key] == starting_car_location:
-            data_depot = key
-            break
-
-    manager = pywrapcp.RoutingIndexManager(len(data_dist), data_num_vehicles, data_depot)
-    routing = pywrapcp.RoutingModel(manager)
-
-    def distance_callback(from_index, to_index):
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return data_dist[from_node][to_node]
-
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-    search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
-    search_parameters.time_limit.seconds = 5 # CHANGE THIS
-    assignment = routing.SolveWithParameters(search_parameters)
-    if not assignment:
-        raise Exception("Nani TSP didn't work!")
-
-    newcycle = []
-    index = routing.Start(0)
-    while not routing.IsEnd(index):
-        newcycle.append(manager.IndexToNode(index))
-        index = assignment.Value(routing.NextVar(index))
-    newcycle.append(manager.IndexToNode(index))
-    newcycle = [dpmap[i] for i in newcycle]
-
-    # Update listlocs
-    listlocs = []
-    curr = -1
-    for i in range(len(newcycle)-1):
-        curr = newcycle[i]
-        while curr != newcycle[i+1]:
-            listlocs.append(curr)
-            curr = pcessors[newcycle[i+1]][curr]
-    listlocs.append(curr)
+    listlocs = optitsp.reconLocs(optitsp.gurobiTspCycle(starting_car_location, set(listdropoffs.keys()), all_paths), pcessors)
 
     t1 = time.perf_counter() - t0
     print(" done! Time: {}s".format(t1))
--- libs/optitsp.py
+++ libs/optitsp.py
@@ -14,6 +14,7 @@ def solve(sloc, stas, G, donttouch=set()):
     ta_cycle = gurobiTspCycle(sloc, stas, alldists)
     drop_cycle = optCycle(ta_cycle, alldists, donttouch)
     listdropoffs = reconDropoffs(sloc, stas, ta_cycle, drop_cycle)
+    drop_cycle = gurobiTspCycle(sloc, set(listdropoffs.keys()), alldists)
     listlocs = reconLocs(drop_cycle, pcessors)
 
     return listlocs, listdropoffs
